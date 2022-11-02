72bafbd3461199c742f48d1cd6dccdf22559c849
cloudsony999 <cloudsony999@gmail.com>
2022-11-02 15:54:13 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test
+{
+	
+	public static void main(String args[])
+	{
+		System.out.println("version is "+System.getProperty("java.version"));
+		System.out.println("OS name is "+System.getProperty("os.name"));
+		System.out.println("Vendor is "+System.getProperty("java.vendor"));
+	}
+}
+
+		
